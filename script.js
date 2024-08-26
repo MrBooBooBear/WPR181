2e4482ecebc4df0ee7f22cca77007b1a7271d708
MrBooBooBear <fouriejooste@gmail.com>
2024-08-27 00:30:15 +0300

    Added popup functionality

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', function() {
+    this.sessionStorage.removeItem('popupShown');
+})
+window.addEventListener('scroll',function() {
+    if (!this.sessionStorage.getItem('popupShown')) {
+        var popupId = this.document.querySelector('.popup').id;
+        if (this.window.scrollY > 400) {
+            this.document.getElementById(popupId).style.display = 'flex';
+            this.document.body.classList.add('noScroll');
+            this.sessionStorage.setItem('popupShown', popupId);
+        }
+    }
+})
+document.querySelectorAll('.close').forEach(function(closeButton) {
+    closeButton.addEventListener('click', function() {
+        var popupId = this.getAttribute('data-popup');
+        var popup = document.getElementById(popupId);
+        if (popup) {
+            popup.style.display = 'none';
+            document.body.classList.remove('noScroll');
+        }
+    });
+})
